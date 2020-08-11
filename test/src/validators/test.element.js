658ac685e2258d19e0618902d12dddf7e66d40ae
Ghustavh Ehm <ghustavh97@gmail.com>
2020-08-11 03:06:51 +0000

    test: complete test

--- test/src/validators/test.element.js
+++ test/src/validators/test.element.js
@@ -2,19 +2,14 @@
 
 const { is, assert, inputs } = require("../helper");
 
-describe("is.element", () => {
-
-});
-
-// test('is.element', function (t) {
-//     t.notOk(is.element(), 'undefined is not element');
-  
-//     t.test('when HTMLElement exists', { skip: typeof HTMLElement === 'undefined' }, function (st) {
-//       var element = document.createElement('div');
-//       st.ok(is.element(element), 'HTMLElement is element');
-//       st.notOk(is.element({ nodeType: 1 }), 'object with nodeType is not element');
-//       st.end();
-//     });
-  
-//     t.end();
-// });
+if (typeof HTMLElement !== "undefined") {
+	describe("is.element", () => {
+		it(`can tell that HTMLElement is an element`, () => {
+			var element = document.createElement("div");
+			assert.isTrue(is.element(element));
+		});
+		it(`can tell that object with nodeType is not element`, () => {
+			assert.isTrue(is.element({ nodeType: 1 }));
+		});
+	});
+}
