bc4a99d0af30e734c14b7542e5da1e2c11085e41
Ghustavh Ehm <ghustavh97@gmail.com>
2020-08-23 01:15:57 +0000

    code: eslint and change heading-depth to 3

--- scripts/docs.build.js
+++ scripts/docs.build.js
@@ -1,16 +1,12 @@
 "use strict";
 
 const fs = require("fs");
-const path = require("path");
 const jsdoc2md = require("jsdoc-to-markdown");
 
 const docs = jsdoc2md.renderSync({
-	files: "src/validators/*.js",
+    files: "src/validators/*.js",
     "global-index-format": "none",
-    // "name-format": "class",
-    // "module-index-format": "none",
-    // "param-list-format": "list",
-    // "property-list-format": "list",
-	separators: true,
+    "heading-depth": 3,
+    separators: true,
 });
 fs.writeFileSync("docs/.internal/validators.md", docs);
