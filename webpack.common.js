8cac9fbd59869abf125b621587cfed33f366a0e2
Ghustavh Ehm <ghustavh97@gmail.com>
2021-05-06 13:45:25 +0200

    pkg: drop support for pre-packaged dist bundles

--- webpack.common.js
+++ webpack.common.js
@@ -5,21 +5,18 @@ const pkg = require("./package.json");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: path.resolve(pkg.main),
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            dry: false,
-            cleanOnceBeforeBuildPatterns: [
-                "!oslllo-validator.js",
-                "!oslllo-validator.min.js",
-            ],
-        }),
-    ],
-    output: {
-        library: "is",
-        libraryTarget: "umd",
-        path: path.resolve(__dirname, "dist"),
-    },
+  entry: {
+    app: path.resolve(pkg.main),
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      dry: false,
+      cleanOnceBeforeBuildPatterns: ["!oslllo-validator.js"],
+    }),
+  ],
+  output: {
+    library: "is",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,12 +5,12 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    output: {
-        filename: `${pkg.name}.js`,
-    },
-    devServer: {
-        contentBase: "./dist",
-    },
+  mode: "development",
+  devtool: "inline-source-map",
+  output: {
+    filename: `${pkg.name}.js`,
+  },
+  devServer: {
+    contentBase: "./build",
+  },
 });
